0b83d0e5857b8028ff3364cc7420d1a89ce0a2c7
varma17 <d.varma.17@mountblue.tech>
2021-08-11 13:46:00 +0530

    add problem2 and testProblem2

---
+++ problem2.js
@@ -0,0 +1,5 @@
+function problem2(inventory){
+    return inventory[inventory.length-1];
+}
+
+export { problem2 };
---
+++ test/testProblem2.js
@@ -0,0 +1,8 @@
+import { problem2 } from '../problem2.js';
+import { inventory } from '../inventory.js';
+var carData = {};
+
+carData = problem2(inventory);
+
+console.log("Last car is a "+carData.car_make+" "+carData.car_model);
+
